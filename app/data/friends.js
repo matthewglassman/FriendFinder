91e0ed4fb63b7b16df7deea058805f4a2d30e59f
matthewglassman <matthewglassmn78@gmail.com>
2017-03-09 14:38:55 -0500

    Fixed an issue in API routing with requiring module data

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,4 +1,4 @@
-[
+var friendsArray = [
 {
 	"name":"George",
 	"photo":"http://pbskids.org/curiousgeorge/tv_schedule/img/tv_george_standing.gif",
@@ -15,3 +15,5 @@
 	1
 	]
 }]
+
+module.exports = friendsArray;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,4 +1,4 @@
-var friendData = require("/../data/friends");
+var friendData = require("../data/friends");
 
 module.exports = function(app){
 	app.get("/api/friends", function(req, res){
@@ -6,11 +6,9 @@ module.exports = function(app){
 	});
 
 	app.post("/api/friends", function(req, res){
-		for (i=0; i < friendData.length; i++){
-			for(j=0; j < friendData[i].score){
-				
-			}
-		}
-
+		var newUser = req.body;
+		console.log(newUser);
 	});
+
+	};
 
