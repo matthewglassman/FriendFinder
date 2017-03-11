18911c86186c3073d9e2fe479b6c68384d5000d9
matthewglassman <matthewglassmn78@gmail.com>
2017-03-10 20:06:54 -0500

    Using a object for placeholder

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,21 +7,29 @@ module.exports = function(app){
 
 	app.post("/api/friends", function(req,res){
 		var surveyTakerInfo = req.body;
-		var friendMatch;
-		var friendsValue;
-		var bestMatch = 40;
+		console.log(req.body);
+		//var friendMatch;
+		//var friendsValue;
+		//var bestMatch = 40;
+		var currentValue = 0;
+		var bestFriend = {
+			name: "",
+			photo: "",
+			bestMatch: 40
+		};
 
 		for (var i = 0; i < friendData.length; i++){
 			currentValue = 0;
 			for (var j = 0; j < friendData[i].scores.length; j++){
 				currentValue+=Math.abs(friendData[i].scores[j] - surveyTakerInfo.scores[j]);
 				console.log(friendData[i].name +" "+currentValue);
-				if (currentValue < bestMatch){
-					bestMatch = currentValue;
-					console.log(bestMatch);
+				if (currentValue < bestFriend.bestMatch){
+					bestFriend.name = friendData[i].name;
+					bestFriend.photo = friends[i].photo;
+					bestFriend.bestMatch = currentValue;
+					console.log(bestFriend);
 				}
 			};
 		};
-		console.log(bestMatch);
 	});
 };
