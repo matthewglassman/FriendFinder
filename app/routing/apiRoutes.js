0bf33022bca7e060087c41d2e16b5be46adbfbff
matthewglassman <matthewglassmn78@gmail.com>
2017-03-12 14:53:09 -0400

    Modal and logic working. Next, add logic to push new friend/survey taker to API

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -15,22 +15,25 @@ module.exports = function(app){
 		var bestFriend = {
 			name: "",
 			photo: "",
-			bestMatch: 40
+//			bestMatch: 40
 		};
+		var bestMatch = 40;
 
 		for (var i = 0; i < friendData.length; i++){
 			currentValue = 0;
 			for (var j = 0; j < friendData[i].scores.length; j++){
 				currentValue+=Math.abs(friendData[i].scores[j] - surveyTakerInfo.scores[j]);
 				console.log(friendData[i].name +" "+currentValue);
-				if (currentValue < bestFriend.bestMatch){
+				
+			};
+		if (currentValue < bestMatch){
 					bestFriend.name = friendData[i].name;
 					bestFriend.photo = friendData[i].photo;
-					bestFriend.bestMatch = currentValue;
+					bestMatch = currentValue;
 					
 				}
-			};
 		};
 		console.log(bestFriend);
+		res.json(bestFriend);
 	});
 };
